609ac2f1afdcf644e630edf6b4bb183993273b32
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-10-13 12:09:39 +0300

    add client

---
+++ project-comments_client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import { Comments } from './components/Comments'
+import { Form } from './components/Form'
+
+function App() {
+  return (
+    <>
+      <Form />
+      <Comments />
+    </>
+  )
+}
+
+export default App
